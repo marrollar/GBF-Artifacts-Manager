0d71df305c0519462be889fcf9dd904ea25f02ba
marrollar <>
2026-04-06 12:07:26 -0400

    Port StorageProxy to typescript.

---
+++ extension/src/StorageProxy.ts
@@ -0,0 +1,99 @@
+import { activeDebuggers } from "./DebuggerManager.js";
+
+/**
+ * Synchronizes read/write events to prevent race conditions
+ */
+class StorageProxy {
+  writeRequestQueue: object[];
+  writeProtectFlag: boolean;
+
+  constructor() {
+    /** A queue of objects waiting to be written to local storage */
+    this.writeRequestQueue = [];
+    /** Set to true when an object in the writeRequestQueue is being processed by processRequest() */
+    this.writeProtectFlag = false;
+    return this;
+  }
+
+  async processRequest() {
+    this.writeProtectFlag = true;
+    while (this.writeRequestQueue.length > 0) {
+      const obj = this.writeRequestQueue.shift();
+      // console.log("SAVING NEW OBJECT: ", obj);
+      if (obj) {
+        await this.saveObjectInLocalStorage(obj);
+      } else {
+        // console.log("FAILED TO SAVE NEW OBJECT")
+      }
+    }
+    this.writeProtectFlag = false;
+  }
+
+  async get(key:string) {
+    const writeRequestQueueCopy = this.writeRequestQueue.slice();
+    while (writeRequestQueueCopy.length > 0) {
+      let object:Record<string, any> | undefined = writeRequestQueueCopy.pop();
+      if (object?.hasOwnProperty(key)) {
+        return object[key];
+      }
+    }
+    return this.getObjectFromLocalStorage(key);
+  }
+
+  async save(obj:object) {
+    /** Updates local values if the values in perminent storage change */
+    // if (obj?.Settings?.timerStart) {
+    //     timerStart = obj.Settings.timerStart;
+    // }
+    // else if (obj?.eventList) {
+    //     eventList = obj.eventList;
+    // }
+
+    this.writeRequestQueue.push(obj);
+    if (!this.writeProtectFlag) {
+      this.processRequest();
+    }
+  }
+
+  async getVar(varName:string) {
+    switch (varName) {
+      case "activeDebuggers":
+        return activeDebuggers;
+      case "Settings":
+        let settings = await storageProxy.get("Settings");
+        return settings;
+      default:
+        return "Error: data did not exist or access is not allowed";
+    }
+  }
+
+  /* Returns object from chrome's local storage */
+  async getObjectFromLocalStorage(key:string) {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.get(key, function (value) {
+          resolve(value[key]);
+        });
+      } catch (ex) {
+        console.log(ex);
+        reject(ex);
+      }
+    });
+  }
+
+  /* Stores object from chrome's local storage */
+  async saveObjectInLocalStorage(obj:object) {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.set(obj, function () {
+          resolve("Saved new object");
+        });
+      } catch (ex) {
+        console.log(ex);
+        reject(ex);
+      }
+    });
+  }
+}
+
+export const storageProxy = new StorageProxy();
