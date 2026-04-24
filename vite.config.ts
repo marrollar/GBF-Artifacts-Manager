203c7a7779a5c36e49e5e48649a5dbdb828afede
marrollar <>
2026-04-24 19:40:53 -0400

    Fix build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  base: "./",
+  build: {
+    minify: true,
+    rolldownOptions: {
+      input: {
+        content: "./index.html",
+        background: "./src/extension/background.ts",
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "content") {
+            return "index.js";
+          }
+          if (chunkInfo.name === "background") {
+            return "background.js";
+          }
+          return "[name].js";
+        },
+      },
+    },
+  },
+});
