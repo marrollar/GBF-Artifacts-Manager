aa7f5bf7fcc6a4d799be2f5aefad8f8ca898969a
marrollar <>
2026-04-24 18:02:22 -0400

    Rescaffold

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
-import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: "./",
-  build: {
-    minify:true, // TODO: If releasing, change this to true
-    rolldownOptions: {
-      input: {
-        content: "./index.html",
-        background: "./src/extension/background.js",
-      },
-      output: {
-        entryFileNames: (chunkInfo) => {
-          if (chunkInfo.name === "content") {
-            return "index.js";
-          }
-          if (chunkInfo.name === "background") {
-            return "background.js";
-          }
-          return "[name].js";
-        },
-      },
-    },
-  },
-});
+  plugins: [vue()],
+})
