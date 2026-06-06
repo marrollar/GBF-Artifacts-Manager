d6ce59820f1e4bb7235bc4d3192e121f9f1d2822
marrollar <>
2026-06-06 06:16:30 -0400

    Fix vite alias configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   base: "./",
+  resolve: {
+    alias: {
+      "@":path.resolve(__dirname, "./src")
+    }
+  },
   build: {
     minify: true,
     rolldownOptions: {
